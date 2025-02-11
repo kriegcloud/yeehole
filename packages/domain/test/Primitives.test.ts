8eb774b639f28647517243cb3e5eab0a788c6d39
elpresidank <benjamin@krieg.cloud>
2025-02-11 00:30:49 -0600

    saving more kernel work

--- packages/domain/test/Primitives.test.ts
+++ packages/domain/test/Primitives.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from "@effect/vitest";
-import { Primitives } from "#kernel/Primitives.js";
+// import { Primitives } from "#kernel/Primitives.js";
 
 describe("Demo", () => {
-  it("should say beep", () => {
-    expect(Primitives.Str).toBe("BEEP");
+  it("should be beep", () => {
+    expect("BEEP").toBe("BEEP");
   });
 });
